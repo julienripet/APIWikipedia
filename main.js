8b6f4908ae00e78f97839927d7d76d16d4efee21
julien_ripet <ripetjulien@gmail.com>
2019-02-20 16:56:42 +0100

    ef

--- main.js
+++ main.js
@@ -1,45 +1,66 @@
-
 window.onload = function(){
+
+
     let section = document.getElementsByTagName("section")[0];
-    document.getElementById("rechercheWiki").placeholder = "Reinventing the wheel"
+    document.getElementById("rechercheWiki").placeholder = "Reinventing the wheel";
+    document.getElementById("rechercheWiki").value = "";
 
     let lanceRequete = function(){
-        //Removes the previous searches
-        if (section.firstChild){
-            section.removeChild(section.firstChild)
-        }
+        let timeTillExecution = 2000;
+        setTimeout(function(){
 
-
-        let mainContainer = document.createElement("div");
-        let request = new XMLHttpRequest();
-        let URL = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
-        let textRequete = document.getElementById("rechercheWiki").value;
-        if (textRequete!=""){
-            request.open("GET",URL+textRequete,false);
-            request.send();
-            let data = JSON.parse(request.responseText);
-            for(i=0;i<data[1].length;i++){
-                // console.log(data[1][i])
-                // console.log(data[2][i])
-                // console.log(data[3][i])
-
-                mainContainer.id= data[0];
-                let title = document.createElement("h2")
-                let text = document.createElement("p")
-                let link = document.createElement("a")
-                title.innerText = data[1][i]
-                text.innerText = data[2][i]
-                link.href = data[3][i]
-
-                section.appendChild(mainContainer);
-                mainContainer.appendChild(link);
-                link.appendChild(title);
-                mainContainer.appendChild(text)
+            //Removes the previous searches
+            if (section.firstChild){
+                section.removeChild(section.firstChild);
             }
+
+            let mainContainer = document.createElement("div");
+
+            //Create the XHR 
+            let request = new XMLHttpRequest();
+            let URL = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
+            let textRequete = document.getElementById("rechercheWiki").value;
+            if (textRequete!=""){
+                request.open("GET",URL+textRequete,true);
+                console.log(textRequete);
+                request.onload=function(e){
+                    console.log(request.readyState + " "+ request.status)
+                    if (request.readyState === 4) {
+                        if (request.status === 200) {
+                            let data = JSON.parse(request.responseText);
+                            
+                            //Loop in data, creates HTML from it
+                            for(i=0;i<data[1].length;i++){
+
+                                mainContainer.id= data[0];
+                                let title = document.createElement("h2");
+                                let text = document.createElement("p");
+                                let link = document.createElement("a");
+
+                                title.innerText = data[1][i];
+                                text.innerText = data[2][i];
+                                link.href = data[3][i];
+
+                                section.appendChild(mainContainer);
+                                mainContainer.appendChild(link);
+                                link.appendChild(title);
+                                mainContainer.appendChild(text);
+                            }
+                        }
+                    }
+                }
+                request.onerror=function(){
+                    section.innerText="Erreur durant la recherche, vérifiez votre connexion internet";
+                }
+                request.send();
+                
+                }
+            },timeTillExecution);
         }
-    }
-    document.getElementById("rechercheWiki").addEventListener("input",lanceRequete)
-    // document.getElementById("rechercheWiki").addEventListener("mouseover",function(){
-    //     document.getElementById("rechercheWiki").value = ""})
+    
+    document.getElementById("rechercheWiki").addEventListener("input",lanceRequete);
+
+
+
 
 }
